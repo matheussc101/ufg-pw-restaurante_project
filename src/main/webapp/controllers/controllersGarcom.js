d975116195ef7aa09248440bbc105d2a68233ade
matheussc101 <matheussc101@gmail.com>
2016-10-12 13:33:57 -0300

    Alterções finais

--- src/main/webapp/controllers/controllersGarcom.js
+++ src/main/webapp/controllers/controllersGarcom.js
@@ -36,5 +36,53 @@ appt.controller('encaminharEntregasConfirmacao', function($rootScope, $location)
 });
 
 appt.controller('telefonista', function($rootScope, $location) {
-	   
 });
+appt.controller('controllersGerenciais', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+});
+
+
+
+appt.controller('cadastrodeCardapio', function($rootScope, $location) {
+	   $rootScope.activetab = $location.path();
+});
+
+appt.controller('pedidosPreparar', function($rootScope, $location) {
+	   $rootScope.activetab = $location.path();
+});
+
+appt.controller('estacionamento', function($rootScope, $location) {
+	   $rootScope.activetab = $location.path();
+});
+appt.controller('relatorioDevolucoes', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+    angular.module('dateInputExample', [])
+        .controller('DateController', ['$scope', function($scope) {
+            $scope.example = {
+                value: new Date(2016, 9, 25)
+            };
+        }]);
+});
+
+appt.controller('relatorioEficiencia', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+    angular.module('dateInputExample', [])
+        .controller('DateController', ['$scope', function($scope) {
+            $scope.example = {
+                value: new Date(2016, 9, 25)
+            };
+        }]);
+});
+
+appt.controller('cadastrarFuncionario', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+});
+
+appt.controller('listarFuncionario', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+});
+
+appt.controller('alterarFuncionario', function($rootScope, $location) {
+    $rootScope.activetab = $location.path();
+});
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,11 +3,20 @@
 <%@ include file="/views/barra_de_menus.html"%>
 
 <title>Restaurante</title>
+
+
 	</head>
 	<body>
 	<div class="container">
-		<div ng-view></div>  
+		<div class="row">
+		<div ng-view></div>
+		</div>  
 	</div>
+	</div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
 	<script src="appt.js"></script>
     <script src="controllers/controllersGarcom.js"></script>
     
@@ -120,9 +129,34 @@
 			});
 		 
 
-		   
-		 
-		 $("body").vegas('play');
+		 $(document).ready(function () {
+			  var trigger = $('.hamburger'),
+			      overlay = $('.overlay'),
+			     isClosed = false;
+
+			    trigger.click(function () {
+			      hamburger_cross();      
+			    });
+
+			    function hamburger_cross() {
+
+			      if (isClosed == true) {          
+			        overlay.hide();
+			        trigger.removeClass('is-open');
+			        trigger.addClass('is-closed');
+			        isClosed = false;
+			      } else {   
+			        overlay.show();
+			        trigger.removeClass('is-closed');
+			        trigger.addClass('is-open');
+			        isClosed = true;
+			      }
+			  }
+			  
+			  $('[data-toggle="offcanvas"]').click(function () {
+			        $('#wrapper').toggleClass('toggled');
+			  });  
+			});
 		 
 			</script>
 			
